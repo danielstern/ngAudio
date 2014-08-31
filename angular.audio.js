dfd5fa9f7c2e95c56aabdec0052425c20ce153eb
unknown <daniel@danielstern.ca>
2014-08-31 14:30:47 -0400

    sound playing in craz refactur

--- angular.audio.js
+++ angular.audio.js
@@ -1,84 +1,102 @@
 angular.module('ngAudio', [])
-.directive('ngAudio', function($compile, $q) {
-    return {
-        restrict: 'AEC',
-        link: function(scope, element, attrs) {
-            /**
+    .directive('ngAudio', function($compile, $q) {
+        return {
+            restrict: 'AEC',
+            link: function(scope, element, attrs) {
+                /**
               If used as an element, automatically convert to attribute form.
             **/
-            if (element[0].nodeName == 'NG-AUDIO') {
-                var audio = angular.element(document.createElement('audio'));
-                audio.attr('ng-audio');
+                if (element[0].nodeName == 'NG-AUDIO') {
+                    var audio = angular.element(document.createElement('audio'));
+                    audio.attr('ng-audio');
 
-                element.attr('id', '');
-                for (var prop in attrs.$attr) {
-                    audio.attr(prop, attrs[prop]);
+                    element.attr('id', '');
+                    for (var prop in attrs.$attr) {
+                        audio.attr(prop, attrs[prop]);
+                    }
+
+                    var el = $compile(audio)(scope);
+                    element.append(audio);
                 }
 
-                var el = $compile(audio)(scope);
-                element.append(audio);
+            },
+            controller: function($scope, $attrs, $element) {
+
+                $element.on('click', function(e) {
+                    ngAudio.play($attrs.ngAudio);
+                });
+
+            },
+        };
+    })
+    .service('localAudioFindingService', function($q, $timeout) {
+
+        this.find = function(id) {
+            var deferred = $q.defer();
+            var $sound = document.getElementById(id);
+            // $timeout(function() {
+            if ($sound) {
+                deferred.resolve($sound);
+            } else {
+                deferred.reject(id);
             }
+            // },2)
 
-        },
-        controller: function($scope, $attrs, $element) {
+            return deferred.promise;
+        }
+    })
 
-            $element.on('click', function(e) {
-                // ngAudio.play($attrs.ngAudio);
+.service('remoteAudioFindingService', function($q) {
+
+    this.find = function(url) {
+        var deferred = $q.defer();
+        var audio = new Audio();
+
+
+        window.__audio = audio;
+
+        // console.log('adding listener to...',audio);
+
+        audio.addEventListener('error', function(e) {
+            console.log('audio errorer...');
+            deferred.reject();
+        });
+
+        audio.addEventListener('canplay', function(e) {
+            deferred.resolve(audio);
+        })
+
+        // bugfix for chrome...
+        setTimeout(function(){
+          audio.src = url;
+        },1);
+
+        return deferred.promise;
+
+    }
+})
+
+.service('cleverAudioFindingService', function($q, localAudioFindingService, remoteAudioFindingService) {
+    this.find = function(id) {
+        var deferred = $q.defer();
+
+        localAudioFindingService.find(id)
+            .then(deferred.resolve, function() {
+                remoteAudioFindingService.find(id)
+                    .then(deferred.resolve, deferred.reject);
             });
 
-        },
-    };
-})
-.service('localAudioFindingService',function(){
-
-  this.find = function(id) {
-    var deferred = $q.defer();
-    var $sound = document.getElementById(soundUrl);
-    if ($sound) {
-      deferred.resolve($sound);  
-    } else {
-      deferred.reject(id);  
+        return deferred.promise;
     }
-    
-    return deferred.promise;
-  }
 })
 
-.service('remoteAudioFindingService',function(){
-
-  this.find = function(url) {
-    var deferred = $q.defer();
-    var audio = new Audio();
-
-    audio.src = uri;
-
-    var interval = setInterval(function() {
-        if (audio.play) {
-            deferred.resolve(audio);
-            clearInterval(interval);
-        }
-    });
-
-    return deferred.promise;
-
-  }
-})
-
-.service('cleverAudioFindingService',function($q,localAudioFindingService,remoteAudioFindingService){
-  this.find = function(id) {
-    var deferred = $q.defer();
-    var local = localAudioFindingService.find(id);
-    var remote = remoteAudioFindingService.find(id);
-    
-    $q.all([local,remote],function(results){
-      console.log("all strategies resolved",results);
-    })
-    return deferred.promise;
-  }
-})
-
-.service('ngAudio',function(){
-  this.play = function(id) {
-    console.log("playing",id);
-  }
+.service('ngAudio', function(cleverAudioFindingService) {
+    this.play = function(id) {
+        console.log("playing", id);
+        cleverAudioFindingService.find(id)
+            .then(function(ngAudioObject) {
+                console.log("resolved,", ngAudioObject);
+                ngAudioObject.play();
+            })
+    }
 })
