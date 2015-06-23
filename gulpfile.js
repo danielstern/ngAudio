855bd54210accc9f33734092703ab713e45c28f7
Code Whisperer <daniel@danielstern.ca>
2015-06-23 19:06:49 -0400

    added automate gulp deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+    gulp.src('./app/**/*')
+        .pipe(gulp.dest('.tmp'));
+    gulp.src('./bower_components/**/*')
+        .pipe(gulp.dest('.tmp/bower_components'));
+    
+    return gulp.src('.tmp/**/*')
+        .pipe(ghPages());
+});
 
 gulp.task('default',function(){
     browserSync.init({
