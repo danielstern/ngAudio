73c6e8dd627cea5b54db6bf722f9e1d4cb8bfc4a
Breixo <breixoam@gmail.com>
2016-05-30 03:29:26 +0200

    Added toFinish callback when there are x seconds to end

--- app/angular.audio.js
+++ app/angular.audio.js
@@ -190,6 +190,11 @@ angular.module('ngAudio', [])
             completeListeners.push(callback);
         }
 
+        var toFinishListeners = [];
+        this.toFinish = function(secs, callback){
+            toFinishListeners.push({'secs': secs, 'callback': callback});
+        }
+
         this.pause = function() {
             $willPause = true;
         };
@@ -385,6 +390,13 @@ angular.module('ngAudio', [])
                             listener(audioObject);
                         })
                     }
+
+                    toFinishListeners.forEach(function(listener) {
+                        if ((audioObject.duration - audioObject.currentTime) <= listener.secs) {
+                            listener.callback(audioObject);
+                            toFinishListeners.shift();
+                        }
+                    })
 
                     if ($looping && audioObject.currentTime >= audioObject.duration) {
                         if ($looping !== true) {
